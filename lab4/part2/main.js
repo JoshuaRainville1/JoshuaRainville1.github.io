337e3650f07c2c73e569b33ecc88fb33e1ed2032
JoshuaRainville1 <156466822+JoshuaRainville1@users.noreply.github.com>
2024-04-04 13:35:14 -0400

    move
    moved the alt section to correct section

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -13,15 +13,15 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
-const alts = {
-  'pic1.jpg' : 'Closeup human eye',
-  'pic2.jpg' : 'Rock',
-  'pic3.jpg' : 'Flowers',
-  'pic4.jpg' : 'Ancient image',
-  'pic5.jpg' : 'Butterfly'
-}
 
 /* Declaring the alternative text for each image file */
+const alts = {
+    'pic1.jpg' : 'Closeup human eye',
+    'pic2.jpg' : 'Rock',
+    'pic3.jpg' : 'Flowers',
+    'pic4.jpg' : 'Ancient image',
+    'pic5.jpg' : 'Butterfly'
+  }
 
 /* Looping through images */
 
