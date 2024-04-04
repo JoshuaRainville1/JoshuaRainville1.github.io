7aa7299ad8fd5cc77d37e7629d958a3ba45de98e
JoshuaRainville1 <156466822+JoshuaRainville1@users.noreply.github.com>
2024-04-04 13:37:59 -0400

    update
    updated looping code

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -26,8 +26,8 @@ const alts = {
 /* Looping through images */
 
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', `images/${image}`);
+newImage.setAttribute('alt', alts[image]);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
